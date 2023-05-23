c758a8219073a07464e6164c7441520aaaa7642c
Sridhara Dasu <SridharaDasu@gmail.com>
2023-05-23 12:54:38 -0500

    Converted to pt, eta, phi

--- print_crystals_n_clusters.py
+++ print_crystals_n_clusters.py
@@ -5,6 +5,8 @@ import vector
 import numpy as np
 import matplotlib.pyplot as plt
 
+crystal_size = 2. * 3.1415927 / 360.
+
 try:
     data = uproot.open(sys.argv[1] + ".root")
 except Exception:
@@ -16,11 +18,14 @@ for event in events.arrays():
     crystal_px = event.ecalTPGs.fP.fX
     crystal_py = event.ecalTPGs.fP.fY
     crystal_pz = event.ecalTPGs.fP.fZ
-    print("Crystal (px, py, pz):", crystal_px, crystal_py, crystal_pz)
+    crystal = vector.awk({"px":crystal_px, "py":crystal_py, "pz":crystal_pz})
+    print("Crystal (pt, eta, phi):", crystal.pt, crystal.eta, crystal.phi)
 
 events = tree["newClusters"]
 for event in events.arrays():
     cluster_px = event.newClusters.fP.fX
     cluster_py = event.newClusters.fP.fY
     cluster_pz = event.newClusters.fP.fZ
-    print("Cluster (px, py, pz): ", cluster_px, cluster_py, cluster_pz)
+    cluster = vector.awk({"px":cluster_px, "py":cluster_py, "pz":cluster_pz})
+    print("Cluster (pt, eta, phi):", cluster.pt, cluster.eta, cluster.phi)
+
